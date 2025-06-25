4341612e7215024409d2629bca9d7e698bf90a16
LabWorksDai <tempdai2025@gmail.com>
2025-06-25 10:56:55 +0530

    Update test.py

--- test.py
+++ test.py
@@ -13,7 +13,7 @@ y_pred = model.predict(X_test)
 
 # Evaluate
 mse = mean_squared_error(y_test, y_pred)
-print(f"Mean Squared Error: {mse:.2f}"
+print(f"Mean Squared Error: {mse:.2f}")
 
 #Print predictes versus actual
 results_df =  pd.DataFrame({'Actual': y_test, 'Predicted': y_pred})
